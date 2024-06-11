56a49e4be831fa4b66b1353f8b345d3fe100d09a
Leonardo Elias <leoelias02@hotmail.com>
2024-06-11 10:41:24 -0300

    :sparkles: move node pools to inside vultr kubernetes resource

--- main.tf
+++ main.tf
@@ -7,23 +7,21 @@ resource "vultr_kubernetes" "k8" {
   label   = "vke-test"
   version = "v1.30.0+1"
 
+  node_pools {
+    node_quantity = 1
+    plan          = "vc2-1c-1gb-sc1"
+    label         = "vke-nodepool"
+  }
+
   provisioner "local-exec" {
     command = "echo ${self.kube_config} > kubeconfig.txt"
   }
-}
-
-resource "vultr_kubernetes_node_pools" "np" {
-  cluster_id    = vultr_kubernetes.k8.id
-  node_quantity = 1
-  plan          = "vc2-1c-1gb-sc1"
-  label         = "vke-nodepool"
 }
 
 output "vultr_kube_config" {
   value     = vultr_kubernetes.k8.kube_config
   sensitive = true
 }
-
 
 # provider "kubernetes" {
 #   config_path = "${path.module}/kubeconfig.txt"
