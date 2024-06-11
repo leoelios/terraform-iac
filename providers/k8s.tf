e162c8a48e224b14c0ffd872839dcc16b500c1fd
Leonardo Elias <leoelias02@hotmail.com>
2024-06-11 00:42:30 -0300

    :fire: remove duplicated namespace

--- providers/k8s.tf
+++ providers/k8s.tf
@@ -9,9 +9,3 @@ resource "kubernetes_namespace" "infraservices" {
 
   depends_on = [vultr_kubernetes.k8]
 }
-
-resource "kubernetes_namespace" "infraservices" {
-  metadata {
-    name = "infraservices"
-  }
-}
