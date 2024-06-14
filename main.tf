b758d4df9249612b7c1089c07dbdd9cff200ccff
Leonardo Elias <leoelias02@hotmail.com>
2024-06-13 22:11:07 -0300

    :sparkles: add conditions

--- main.tf
+++ main.tf
@@ -129,6 +129,8 @@ resource "helm_release" "cert_manager" {
 }
 
 resource "kubernetes_manifest" "letsencrypt_issuer" {
+  count = length(vultr_kubernetes.k8) > 0 ? 1 : 0
+
   manifest = {
     apiVersion = "cert-manager.io/v1"
     kind       = "ClusterIssuer"
